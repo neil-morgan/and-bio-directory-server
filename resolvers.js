9cd5e069c13c517cb0204c0c207ea3eff3384144
Neil Morgan <neilmorgan@J7435Q42QL.local>
2022-03-17 14:27:29 +0000

    update resolver

--- resolvers.js
+++ resolvers.js
@@ -21,16 +21,15 @@ const resolvers = {
   },
 
   Mutation: {
-    createUser: (_, args) => {
-      const user = args.input;
+    createUser: (_, { input }) => {
+      const user = input;
       const lastId = users[users.length - 1].id;
       user.id = lastId + 1;
       users.push(user);
       return user;
     },
 
-    deleteUser: (_, args) => {
-      const { id } = args;
+    deleteUser: (_, { id }) => {
       remove(users, (user) => user.id === Number(id));
       return null;
     },
