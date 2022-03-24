c6c8bd9b5a061f59141567cf5d330319038e9ce3
Neil Morgan <neilmorgan@J7435Q42QL.local>
2022-03-24 15:02:30 +0000

    small refactors and fixes

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,7 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const find = require("lodash/find");
+
 const serviceAccount = require("./firebase-setup.json");
 
 initializeApp({
@@ -8,21 +9,21 @@ initializeApp({
 });
 
 const db = getFirestore();
-const usersCol = db.collection('users') // Col for collection
+const usersCol = db.collection("users"); // Col for collection
 
-const snapshotToArray = (snapshot) => {
-  let array = [];
+const snapshotToArray = snapshot => {
+  const array = [];
   snapshot.forEach(doc => {
     array.push({ id: doc.id, ...doc.data() });
   });
   return array;
-}
+};
 
 const resolvers = {
   Query: {
     users: async () => {
       const snapshot = await usersCol.get();
-      return snapshotToArray(snapshot)
+      return snapshotToArray(snapshot);
     },
 
     user: async (_, { id }) => {
@@ -33,32 +34,31 @@ const resolvers = {
     userByName: async (_, args) => {
       const { name } = args;
       const snapshot = await usersCol.get();
-      return find(snapshotToArray(snapshot), { name })
+      return find(snapshotToArray(snapshot), { name });
     }
   },
 
   Mutation: {
     createUser: async (_, { input }) => {
       const res = await usersCol.add({ ...input });
-      let user = { id: res.id, ...input }
-      return user;
+      return { id: res.id, ...input };
     },
 
     updateUser: async (_, args) => {
       const { input } = args;
-      let newUserData = input
       const user = usersCol.doc(input.id);
-      delete newUserData.id
-      const res = await user.set({
-        ...newUserData
-      }, { merge: true });
+      const res = await user.set({ ...input }, { merge: true });
 
-      if (res) return newUserData
+      if (res) {
+        return input;
+      }
     },
 
     deleteUser: async (_, { id }) => {
       const res = await usersCol.doc(id).delete();
-      if (res) return null
+      if (res) {
+        return null;
+      }
     }
   }
 };
