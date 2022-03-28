8c2b195a99d53dea68ef0de5b3a67398cdc9dfda
Neil Morgan <neilmorgan@J7435Q42QL.broadband>
2022-03-28 22:14:38 +0100

    type for seniority

--- types.js
+++ types.js
@@ -5,36 +5,37 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
   input createUserData {
     name: String!
-    surname: String!
     role: String!
+    surname: String!
   }
 
   input updateUserData {
     id: ID!
     name: String!
-    surname: String!
     role: String!
+    surname: String!
   }
 
   type User {
     id: ID!
     name: String!
-    surname: String!
     role: String!
-    traits: [String]!
+    seniority: String!
     skills: [String]!
+    surname: String!
+    traits: [String]!
   }
 
   type Query {
-    users: [User!]!
     user(id: ID!): User!
     userByName(name: String!): User!
+    users: [User!]!
   }
 
   type Mutation {
     createUser(input: createUserData!): User
-    updateUser(input: updateUserData!): User
     deleteUser(id: ID!): User
+    updateUser(input: updateUserData!): User
   }
 `;
 
